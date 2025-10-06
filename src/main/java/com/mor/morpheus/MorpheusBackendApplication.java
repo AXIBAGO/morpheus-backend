19535486455044c9c9568b77e2ce8175154d7e53
axibago <1147204871@qq.com>
2025-10-06 15:37:42 +0800

    初始化 Spring Boot 项目结构

---
+++ src/main/java/com/mor/morpheus/MorpheusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mor.morpheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorpheusBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MorpheusBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mor/morpheus/MorpheusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mor.morpheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorpheusBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
